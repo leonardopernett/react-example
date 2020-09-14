5dd1fc659c1f6523f0aa971209d4cb5575fc4d67
leonardo pernett <lnard.dev@gmail.com>
2020-09-14 23:50:10 +0200

    Updates

---
+++ precache-manifest.888693bde22ba11439c78c1e63f0fa27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eaabe6bc0dab40d1d42590d6f44ce2c",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "7a4c0f904830c9e4dc75",
+    "url": "/react-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0babbde14e333bc689a2",
+    "url": "/react-example/static/js/2.8aaac6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-example/static/js/2.8aaac6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4c0f904830c9e4dc75",
+    "url": "/react-example/static/js/main.761441d7.chunk.js"
+  },
+  {
+    "revision": "4debc9966006f7e08404",
+    "url": "/react-example/static/js/runtime-main.3042e55b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
